7ac438726a41173db41bcf1e30adb9636e682ad5
Benoit <benoit@kestra.io>
2023-07-18 12:33:05 +0200

    [feature] add argparse, add utm source and other details

--- dataset/produce/main.py
+++ dataset/produce/main.py
@@ -2,6 +2,10 @@ from faker import Faker
 from faker.providers import BaseProvider
 import uuid
 import random
+from math import pi, sin, cos
+import datetime
+import csv
+import argparse
 
 fake = Faker()
 
@@ -12,7 +16,8 @@ class ShinyRockProvier(BaseProvider):
             "user_id": uuid.uuid4().hex,
             "order_id": uuid.uuid4().hex,
             "order_date": order_date,
-            "product_id": int(select_string({"3": 0.64, "5": 0.36}))
+            "product_id": int(select_string({"3": 0.64, "5": 0.36})),
+            "utm_source": select_string({"_instagram": 0.2, "_amazon": 0.6, "_facebook": 0.1, "_youtube": 0.1})
         }
 
     def payment(self, order_id, product_id):
@@ -29,7 +34,7 @@ class ShinyRockProvier(BaseProvider):
             "user_id": user_id,
             "product_id": product_id,
             "duration": get_random_number(range_a=5, range_b=3600),
-            "date": date
+            "date": date   
         }
 
 
@@ -50,23 +55,45 @@ def select_string(probabilities):
     # If no string is found (e.g., due to rounding errors), return None
     return None
 
+def pattern_function(x, amplitude):
+    v = -6.1
+    t = 1.4
+    return int(max(0, 25 * sin(2*pi*x/200 + pi/2) * v * sin(3*pi*x/500+pi/2) * t * cos(pi*x/100 + pi/3) * t + amplitude))
+
+def date_to_int(date):
+    dt = datetime.datetime.strptime(date, "%Y-%m-%d")
+    return dt.timetuple().tm_yday
+
+def write_list_of_dicts_to_csv(list_of_dicts, filename):
+  with open(filename, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile, delimiter=",")
+    header = list(list_of_dicts[0].keys())
+    writer.writerow(header)
+    for dict in list_of_dicts:
+      row = []
+      for key, value in dict.items():
+        row.append(str(value))
+      writer.writerow(row)
+
 fake.add_provider(ShinyRockProvier)
 
 orders = []
 order_date = "2023-07-01"
-for i in range(0, get_random_number()):
+int_day = date_to_int(order_date)
+nb_order = pattern_function(int_day, 800)
+print(order_date, int_day, nb_order)
+for i in range(0, nb_order):
     order = fake.order(order_date=order_date)
     orders.append(order)
 
-print(orders)
-
+write_list_of_dicts_to_csv(orders, "orders.csv")
 
 payments = []
 for order in orders:
     payment = fake.payment(order.get("order_id"), order.get("product_id"))
     payments.append(payment)
 
-print(payments)
+write_list_of_dicts_to_csv(payments, "payments.csv")
 
 services = []
 for order in orders:
@@ -74,4 +101,10 @@ for order in orders:
         service_run = fake.service(order.get("user_id"), order.get("product_id"), order_date)
         services.append(service_run)
 
-print(services)
+write_list_of_dicts_to_csv(services, "services.csv")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Produce data for the Shiny Rock fictional company")
+    parser.add_argument("--date", help="The production date")
+    args = parser.parse_args()
+
