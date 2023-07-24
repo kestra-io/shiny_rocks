66512eb9ef5e9ede11e4369640c5586d1f5a77bd
Benoit <benoit@kestra.io>
2023-07-24 15:23:06 +0200

    [feature] add chart with python/bq

--- shiny_rocks_dbt/models/mart/mart_marketing_vs_sales.sql
+++ shiny_rocks_dbt/models/mart/mart_marketing_vs_sales.sql
@@ -1,28 +1,28 @@
 
 WITH orders_utm_values AS (
     SELECT
-        orders.month_year,
+        orders.order_date,
         orders.utm_source,
         SUM(payments.amount) AS sum_payments_amount
     FROM {{ ref("staging_orders")}} AS orders
     INNER JOIN {{ ref("staging_payments") }} AS payments
     ON orders.order_id = payments.order_id
-    GROUP BY orders.month_year, orders.utm_source
+    GROUP BY orders.order_date, orders.utm_source
 )
 
 , marketing_sales_agg AS (
     SELECT
-        marketing_month_agg.month_year,
+        date,
         platform,
-        sum_amount AS marketing_cost,
+        amount AS marketing_cost,
         sum_payments_amount AS sales_amount
-    FROM {{ ref("mart_marketing_month_agg")}} AS marketing_month_agg
+    FROM {{ ref("staging_marketing_investments") }} AS marketing_month_agg
     LEFT JOIN orders_utm_values
-    ON marketing_month_agg.month_year = orders_utm_values.month_year AND marketing_month_agg.platform = orders_utm_values.utm_source
+    ON marketing_month_agg.date = orders_utm_values.order_date AND marketing_month_agg.platform = orders_utm_values.utm_source
 )
 
 SELECT
-    month_year,
+    date,
     platform,
     marketing_cost,
     sales_amount,
