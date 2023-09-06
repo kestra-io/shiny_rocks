c40cb6a418a3ab09bb4c054d91e72654bdbb3445
Benoit <benoit@kestra.io>
2023-09-06 15:35:12 +0200

    [fix] string fix

--- shiny_rocks_dbt/models/mdn_staging/staging_marketing_investments.sql
+++ shiny_rocks_dbt/models/mdn_staging/staging_marketing_investments.sql
@@ -1,6 +1,6 @@
 SELECT
     date,
     CONCAT(CAST(EXTRACT(YEAR FROM DATE (date)) AS STRING),'-',CAST(EXTRACT(MONTH FROM DATE (date)) AS STRING)) AS month_year,
-    CONCAT("_", platform) AS platform,
+    CONCAT('_', platform) AS platform,
     amount
 FROM {{ source('marketing', 'marketing_investments') }}
