d7a63b177b13db3ac6d48290a0c9c9b9d566eaf2
Benoit <benoit@kestra.io>
2023-09-06 15:56:04 +0200

    [feature] parse date

--- shiny_rocks_dbt/models/mdn_staging/staging_orders.sql
+++ shiny_rocks_dbt/models/mdn_staging/staging_orders.sql
@@ -1,6 +1,7 @@
 
 
 SELECT
-    *,
-    CONCAT(CAST(EXTRACT(YEAR FROM DATE (order_date)) AS STRING),'-',CAST(EXTRACT(MONTH FROM DATE (order_date)) AS STRING)) AS month_year
+    * EXCLUDE(order_date),
+    TO_DATE(order_date, 'YYYY-MM-DD') AS order_date,
+    CONCAT(CAST(EXTRACT(YEAR FROM DATE (TO_DATE(order_date, 'YYYY-MM-DD'))) AS STRING),'-',CAST(EXTRACT(MONTH FROM DATE (TO_DATE(order_date, 'YYYY-MM-DD'))) AS STRING)) AS month_year
 FROM {{ source('app_log', 'orders') }}
--- shiny_rocks_dbt/models/mdn_staging/staging_services.sql
+++ shiny_rocks_dbt/models/mdn_staging/staging_services.sql
@@ -1,5 +1,6 @@
 
 
 SELECT
-    *
+    * EXCLUDE(run_date),
+    TO_DATE(run_date, 'YYYY-MM-DD') AS run_date
 FROM {{ source('app_log', 'services') }}
