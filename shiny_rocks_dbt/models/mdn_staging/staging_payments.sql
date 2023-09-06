392a28611f2fb460f25f1f2c6e258a493d60dc34
Benoit <benoit@kestra.io>
2023-09-06 15:25:02 +0200

    [fix] fix string delimiter

--- shiny_rocks_dbt/models/mdn_staging/staging_payments.sql
+++ shiny_rocks_dbt/models/mdn_staging/staging_payments.sql
@@ -2,5 +2,5 @@
 
 SELECT
     *,
-    CONCAT(CAST(EXTRACT(YEAR FROM DATE (order_date)) AS STRING),"-",CAST(EXTRACT(MONTH FROM DATE (order_date)) AS STRING)) AS month_year
+    CONCAT(CAST(EXTRACT(YEAR FROM DATE (order_date)) AS STRING),'-',CAST(EXTRACT(MONTH FROM DATE (order_date)) AS STRING)) AS month_year
 FROM {{ source('app_log', 'payments') }}
