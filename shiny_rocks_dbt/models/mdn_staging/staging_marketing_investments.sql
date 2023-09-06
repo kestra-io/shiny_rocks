3acb6a2476df04ecc0cfa3cfb00b1607e09eb79b
Benoit <benoit@kestra.io>
2023-09-06 15:58:52 +0200

    [fix] fix date parsing from spreadsheet

--- shiny_rocks_dbt/models/mdn_staging/staging_marketing_investments.sql
+++ shiny_rocks_dbt/models/mdn_staging/staging_marketing_investments.sql
@@ -1,6 +1,6 @@
 SELECT
-    TO_DATE(date, 'YYYY-MM-DD') AS date,
-    CONCAT(CAST(EXTRACT(YEAR FROM DATE (TO_DATE(date, 'YYYY-MM-DD'))) AS STRING),'-',CAST(EXTRACT(MONTH FROM DATE (TO_DATE(date, 'YYYY-MM-DD'))) AS STRING)) AS month_year,
+    TO_DATE(date, 'YYYY/MM/DD') AS date,
+    CONCAT(CAST(EXTRACT(YEAR FROM DATE (TO_DATE(date, 'YYYY/MM/DD'))) AS STRING),'-',CAST(EXTRACT(MONTH FROM DATE (TO_DATE(date, 'YYYY/MM/DD'))) AS STRING)) AS month_year,
     CONCAT('_', platform) AS platform,
     amount
 FROM {{ source('marketing', 'marketing_investments') }}
